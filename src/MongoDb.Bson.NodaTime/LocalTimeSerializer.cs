c6b052f50af494e123292aa8ddeac482028e5bba
Greg Lincoln <greg@greglincoln.com>
2016-01-06 13:45:17 -0500

    Add support for LocalTime.

---
+++ src/MongoDb.Bson.NodaTime/LocalTimeSerializer.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+using NodaTime.Text;
+
+namespace MongoDb.Bson.NodaTime
+{
+    public class LocalTimeSerializer : PatternSerializerBase<LocalTime>
+    {
+        public LocalTimeSerializer() : base(LocalTimePattern.ExtendedIsoPattern)
+        {}
+    }
+}
---
+++ test/MongoDb.Bson.NodaTime.Tests/LocalTimeSerializerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using NodaTime;
+using Xunit;
+using FluentAssertions;
+
+namespace MongoDb.Bson.NodaTime.Tests
+{
+    public class LocalTimeSerializerTests
+    {
+        static LocalTimeSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer(new LocalTimeSerializer());
+        }
+
+        [Fact]
+        public void CanRoundTripValueWithIsoCalendar()
+        {
+            var obj = new Test { LocalTime = new LocalTime(13, 25, 1) };
+            obj.ToTestJson().Should().Contain("'LocalTime' : '13:25:01'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.LocalTime.Should().Be(obj.LocalTime);
+        }
+
+        [Fact]
+        public void ThrowsWhenDateIsInvalid()
+        {
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalTime", "bleh"))));
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalTime", BsonNull.Value))));
+        }
+
+        [Fact]
+        public void CanParseNullable()
+        {
+            BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("NullableLocalTime", BsonNull.Value))).NullableLocalTime.Should().BeNull();
+        }
+
+        private class Test
+        {
+            public LocalTime LocalTime { get; set; }
+
+            public LocalTime? NullableLocalTime { get; set; }
+        }
+    }
+}
