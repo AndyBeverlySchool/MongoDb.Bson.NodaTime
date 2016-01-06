76630d02e682145af9186975149aec957b5911a5
Greg Lincoln <greg@greglincoln.com>
2016-01-06 11:32:07 -0500

    Add LocalDateSerializer.

---
+++ src/MongoDb.Bson.NodaTime/LocalDateSerializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using NodaTime;
+using NodaTime.Text;
+
+namespace MongoDb.Bson.NodaTime
+{
+    public class LocalDateSerializer : SerializerBase<LocalDate>
+    {
+        public override LocalDate Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var type = context.Reader.GetCurrentBsonType();
+            switch (type)
+            {
+                case BsonType.Array:
+                    context.Reader.ReadStartArray();
+                    var calendarId = context.Reader.ReadString();
+                    var localDate = ParseDatePattern(context.Reader.ReadString());
+                    context.Reader.ReadEndArray();
+
+                    return localDate.WithCalendar(CalendarSystem.ForId(calendarId));
+                case BsonType.String:
+                    return ParseDatePattern(context.Reader.ReadString());
+                default:
+                    throw new NotSupportedException($"Cannot convert a {type} to a LocalDate.");
+            }
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, LocalDate value)
+        {
+            if (value.Calendar != CalendarSystem.Iso)
+            {
+                context.Writer.WriteStartArray();
+                context.Writer.WriteString(value.Calendar.Id);
+                context.Writer.WriteString(LocalDatePattern.IsoPattern.Format(value));
+                context.Writer.WriteEndArray();
+            }
+            else
+            {
+                context.Writer.WriteString(LocalDatePattern.IsoPattern.Format(value));
+            }
+        }
+
+        private static LocalDate ParseDatePattern(string datePattern)
+        {
+            var localDate = LocalDatePattern.IsoPattern.Parse(datePattern);
+            if (!localDate.Success)
+            {
+                throw localDate.Exception;
+            }
+
+            return localDate.Value;
+        }
+    }
+}
---
+++ test/MongoDb.Bson.NodaTime.Tests/LocalDateSerializerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using NodaTime;
+using Xunit;
+
+namespace MongoDb.Bson.NodaTime.Tests
+{
+    public class LocalDateSerializerTests
+    {
+        static LocalDateSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer(new LocalDateSerializer());
+        }
+
+        [Fact]
+        public void CanRoundTripValueWithIsoCalendar()
+        {
+            var obj = new Test { LocalDate = new LocalDate(2015, 1, 1) };
+            obj.ToTestJson().Should().Contain("'LocalDate' : '2015-01-01'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.LocalDate.Should().Be(obj.LocalDate);
+        }
+
+        [Fact]
+        public void CanRoundTripWithAlternativeCalendar()
+        {
+            var obj = new Test { LocalDate = new LocalDate(2015, 1, 1).WithCalendar(CalendarSystem.GetPersianCalendar()) };
+            obj.ToTestJson().Should().Contain("'LocalDate' : ['Persian', '1393-10-11']");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.LocalDate.Should().Be(obj.LocalDate);
+        }
+
+        [Fact]
+        public void ThrowsWhenDateIsInvalid()
+        {
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalDate", "bleh"))));
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalDate", BsonNull.Value))));
+        }
+
+        [Fact]
+        public void CanParseNullable()
+        {
+            BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("NullableLocalDate", BsonNull.Value))).NullableLocalDate.Should().BeNull();
+        }
+
+        private class Test
+        {
+            public LocalDate LocalDate { get; set; }
+
+            public LocalDate? NullableLocalDate { get; set; }
+        } 
+    }
+}
