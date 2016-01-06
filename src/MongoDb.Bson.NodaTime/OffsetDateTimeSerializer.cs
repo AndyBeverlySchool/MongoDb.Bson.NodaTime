ef86e9442dc8fb078cfe03ae791459aa3e1a9210
Greg Lincoln <greg@greglincoln.com>
2016-01-06 12:44:26 -0500

    Add support for OffsetDateTime.

---
+++ src/MongoDb.Bson.NodaTime/OffsetDateTimeSerializer.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+using NodaTime.Text;
+
+namespace MongoDb.Bson.NodaTime
+{
+    public class OffsetDateTimeSerializer : PatternSerializerBase<OffsetDateTime>
+    {
+        public OffsetDateTimeSerializer() : base(OffsetDateTimePattern.ExtendedIsoPattern, d => d.WithCalendar(CalendarSystem.Iso))
+        { }
+    }
+}
---
+++ test/MongoDb.Bson.NodaTime.Tests/OffsetDateTimeSerializer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using NodaTime;
+using Xunit;
+using FluentAssertions;
+
+namespace MongoDb.Bson.NodaTime.Tests
+{
+    public class OffsetDateTimeSerializerTests
+    {
+        static OffsetDateTimeSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer(new OffsetDateTimeSerializer());
+        }
+
+        [Fact]
+        public void CanRoundTripValueWithIsoCalendar()
+        {
+            var obj = new Test { OffsetDateTime = new LocalDateTime(2015, 1, 2, 3, 4, 5).WithOffset(Offset.FromHours(1)) };
+            obj.ToTestJson().Should().Contain("'OffsetDateTime' : '2015-01-02T03:04:05+01'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.OffsetDateTime.Should().Be(obj.OffsetDateTime);
+        }
+
+
+        [Fact]
+        public void ConvertsToIsoCalendarWhenSerializing()
+        {
+            var obj = new Test { OffsetDateTime = new LocalDateTime(2015, 1, 2, 3, 4, 5).WithOffset(Offset.FromHours(1)).WithCalendar(CalendarSystem.GetPersianCalendar()) };
+            obj.ToTestJson().Should().Contain("'OffsetDateTime' : '2015-01-02T03:04:05+01'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.OffsetDateTime.Should().Be(obj.OffsetDateTime.WithCalendar(CalendarSystem.Iso));
+        }
+
+        [Fact]
+        public void ThrowsWhenDateIsInvalid()
+        {
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("OffsetDateTime", "bleh"))));
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("OffsetDateTime", BsonNull.Value))));
+        }
+
+        [Fact]
+        public void CanParseNullable()
+        {
+            BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("NullableOffsetDateTime", BsonNull.Value))).NullableOffsetDateTime.Should().BeNull();
+        }
+
+        private class Test
+        {
+            public OffsetDateTime OffsetDateTime { get; set; }
+
+            public OffsetDateTime? NullableOffsetDateTime { get; set; }
+        }
+    }
+}
