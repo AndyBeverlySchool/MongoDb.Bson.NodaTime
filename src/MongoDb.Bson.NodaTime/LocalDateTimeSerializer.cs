6f2e028205d41d73960a7e4bed8d7de5bc164b81
Greg Lincoln <greg@greglincoln.com>
2016-01-06 12:16:58 -0500

    Add LocalDateTimeSerializer.

---
+++ src/MongoDb.Bson.NodaTime/LocalDateTimeSerializer.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+using NodaTime.Text;
+
+namespace MongoDb.Bson.NodaTime
+{
+    public class LocalDateTimeSerializer : PatternSerializerBase<LocalDateTime>
+    {
+        public LocalDateTimeSerializer() : base(LocalDateTimePattern.ExtendedIsoPattern, d => d.WithCalendar(CalendarSystem.Iso))
+        {}
+    }
+}
---
+++ test/MongoDb.Bson.NodaTime.Tests/LocalDateTimeSerializerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using NodaTime;
+using Xunit;
+
+namespace MongoDb.Bson.NodaTime.Tests
+{
+    public class LocalDateTimeSerializerTests
+    {
+        static LocalDateTimeSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer(new LocalDateTimeSerializer());
+        }
+
+        [Fact]
+        public void CanRoundTripValueWithIsoCalendar()
+        {
+            var obj = new Test { LocalDateTime = new LocalDateTime(2015, 1, 2, 3, 4, 5) };
+            obj.ToTestJson().Should().Contain("'LocalDateTime' : '2015-01-02T03:04:05'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.LocalDateTime.Should().Be(obj.LocalDateTime);
+        }
+
+
+        [Fact]
+        public void ConvertsToIsoCalendarWhenSerializing()
+        {
+            var obj = new Test { LocalDateTime = new LocalDateTime(2015, 1, 2, 3, 4, 5).WithCalendar(CalendarSystem.GetPersianCalendar()) };
+            obj.ToTestJson().Should().Contain("'LocalDateTime' : '2015-01-02T03:04:05'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.LocalDateTime.Should().Be(obj.LocalDateTime.WithCalendar(CalendarSystem.Iso));
+        }
+
+        [Fact]
+        public void ThrowsWhenDateIsInvalid()
+        {
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalDateTime", "bleh"))));
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("LocalDateTime", BsonNull.Value))));
+        }
+
+        [Fact]
+        public void CanParseNullable()
+        {
+            BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("NullableLocalDateTime", BsonNull.Value))).NullableLocalDateTime.Should().BeNull();
+        }
+
+        private class Test
+        {
+            public LocalDateTime LocalDateTime { get; set; }
+
+            public LocalDateTime? NullableLocalDateTime { get; set; }
+        }
+    }
+}
