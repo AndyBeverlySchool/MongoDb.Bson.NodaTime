720ce98fb28b9129826a75fdfccac0eec9361396
Greg Lincoln <greg@greglincoln.com>
2016-01-06 13:26:01 -0500

    Rename serializer class.

--- src/MongoDb.Bson.NodaTime/NodaTimeSerializers.cs
+++ src/MongoDb.Bson.NodaTime/NodaTimeSerializers.cs
@@ -5,11 +5,11 @@ using MongoDB.Bson.Serialization;
 
 namespace MongoDb.Bson.NodaTime
 {
-    public static class Serializers
+    public static class NodaTimeSerializers
     {
         public static void Register()
         {
-            var classes = typeof(Serializers).GetTypeInfo().Assembly.DefinedTypes
+            var classes = typeof(NodaTimeSerializers).GetTypeInfo().Assembly.DefinedTypes
                 .Where(t => t.BaseType != null && !t.ContainsGenericParameters && t.ImplementedInterfaces.Contains(typeof(IBsonSerializer)))
                 .ToList();
             classes.ForEach(t => BsonSerializer.RegisterSerializer(t.BaseType.GenericTypeArguments[0], Activator.CreateInstance(t.AsType()) as IBsonSerializer));
