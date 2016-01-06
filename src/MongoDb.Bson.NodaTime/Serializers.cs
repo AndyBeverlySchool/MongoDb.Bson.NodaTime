0d439d0a06911d1ef54e1adf29f73be155eaa391
Greg Lincoln <greg@greglincoln.com>
2016-01-06 13:19:56 -0500

    Add bootstrap method.

---
+++ src/MongoDb.Bson.NodaTime/Serializers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDb.Bson.NodaTime
+{
+    public static class Serializers
+    {
+        public static void Register()
+        {
+            var classes = typeof(Serializers).GetTypeInfo().Assembly.DefinedTypes
+                .Where(t => t.BaseType != null && !t.ContainsGenericParameters && t.ImplementedInterfaces.Contains(typeof(IBsonSerializer)))
+                .ToList();
+            classes.ForEach(t => BsonSerializer.RegisterSerializer(t.BaseType.GenericTypeArguments[0], Activator.CreateInstance(t.AsType()) as IBsonSerializer));
+        }
+    }
+}
