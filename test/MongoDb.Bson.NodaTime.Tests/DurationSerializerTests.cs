ad5aa90694328790fa44ee2359aa5967a89d0e2c
Etienne Maheu <etienne.maheu@radio-canada.ca>
2016-07-28 10:31:10 -0400

    Add support for NodaTime.Duration

---
+++ test/MongoDb.Bson.NodaTime.Tests/DurationSerializerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using NodaTime;
+using Xunit;
+
+namespace MongoDb.Bson.NodaTime.Tests
+{
+    public class DurationSerializerTests
+    {
+        static DurationSerializerTests()
+        {
+            BsonSerializer.RegisterSerializer(new DurationSerializer());
+        }
+
+        [Fact]
+        public void CanConvertValue()
+        {
+            var obj = new Test { Duration = Duration.FromSeconds(34) };
+            obj.ToTestJson().Should().Contain("'Duration' : '0:00:00:34'");
+
+            obj = BsonSerializer.Deserialize<Test>(obj.ToBson());
+            obj.Duration.Should().Be(obj.Duration);
+        }
+
+
+        [Fact]
+        public void ThrowsWhenValueIsInvalid()
+        {
+            Assert.Throws<FormatException>(() => BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("Duration", "bleh"))));
+        }
+
+        [Fact]
+        public void CanParseNullable()
+        {
+            BsonSerializer.Deserialize<Test>(new BsonDocument(new BsonElement("Duration", BsonNull.Value))).Duration.Should().BeNull();
+        }
+
+        private class Test
+        {
+            public Duration Duration { get; set; }
+        }
+    }
+}
