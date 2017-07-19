1e5046c3b6c3adf4a9d37c4f6dd0dfc510f384f0
Greg Lincoln <greg@greglincoln.com>
2017-07-19 16:28:38 -0400

    Remove unneeded PCL shims. Update NodaTime.Testing. Switch test project to .netcoreapp1.1.

--- src/MongoDb.Bson.NodaTime/ZonedDateTimeSerializer.cs
+++ src/MongoDb.Bson.NodaTime/ZonedDateTimeSerializer.cs
@@ -5,7 +5,7 @@ namespace MongoDb.Bson.NodaTime
 {
     public class ZonedDateTimeSerializer : PatternSerializer<ZonedDateTime>
     {
-        private static readonly IPattern< ZonedDateTime> Pattern = ZonedDateTimePattern.CreateWithInvariantCulture("G", DateTimeZoneProviders.Tzdb);
+        private static readonly IPattern<ZonedDateTime> Pattern = ZonedDateTimePattern.CreateWithInvariantCulture("G", DateTimeZoneProviders.Tzdb);
         public ZonedDateTimeSerializer() : base(Pattern)
         { }
     }
